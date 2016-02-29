5269b62e0a7749d9b2f4e6df541f9a91c4b69c14
Rebecca Long <rebecca.long@gmail.com>
2016-02-28 21:47:35 -0800

    Initial commit for repo

---
+++ src/RebeccaLong.Web/client/app.js
@@ -0,0 +1,39 @@
+﻿(function(angular) {
+    'use strict';
+
+    angular
+        .module('portfolio', ['ui.router'])
+        .config(config)
+        .run(run);
+
+    config.$inject = ['$stateProvider', '$locationProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode(true)
+            .hashPrefix('!');
+
+        $stateProvider
+            .state('main', {
+                templateUrl: 'client/main/main.html',
+                controller: 'MainCtrl as vm'
+            });
+    }
+
+    run.$inject = ['$state', '$rootScope'];
+
+    function run($state, $rootScope) {
+        $rootScope.$on('stateChangeStart',
+            function (event, toState, toParams, fromState, fromParams, options) {
+                console.log(toState.name);
+            });
+
+        $rootScope.$on('stateChangeError',
+            function (event, toState, toParams, fromState, fromParams, error) {
+                console.log(toState.name);
+            });
+
+        $state.go('main');
+    }
+
+})(window.angular);
---
+++ src/RebeccaLong.Web/client/main/mainCtrl.js
@@ -0,0 +1,20 @@
+﻿(function(angular) {
+    angular.
+        module('portfolio').
+        controller('MainCtrl', mainCtrl);
+
+    mainCtrl.$inject = ['$state'];
+
+    function mainCtrl($state) {
+        var vm = this;
+
+        console.log('Should work');
+
+        vm.navigate = navigate;
+
+        function navigate() {
+            $state.go('main');
+        }
+    }
+
+})(window.angular);
